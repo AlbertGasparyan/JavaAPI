043c290c12f5dc95a56e49841ceec34c8f404a79
Albert <mona-m1@ya.ru>
2023-03-13 11:45:13 +0600

    mod task

--- HomeWork/Task05.java
+++ HomeWork/Task05.java
@@ -5,12 +5,15 @@ public class Task05 {
         int num1 = 3;
         int num2 = 56;
         String plus = "равно";
+        long begin = System.currentTimeMillis();
 
         StringBuilder text = new StringBuilder();
         text.append(String.format("%d + %d = %d\n", num1,num2,num1+num2));
         text.append(String.format("%d * %d = %d\n", num1,num2,num1*num2));
         text.append(String.format("%d - %d = %d\n", num1,num2,num1-num2));
         text.append(String.format("%d / %d = %d\n", num1,num2,num1/num2));
+        long end = System.currentTimeMillis();
         System.out.println(text.toString().replaceAll("=", "равно"));
+        System.out.println(end-begin+"ms");
     }
 }
